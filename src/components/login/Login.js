5556531d4c90d7ea0a4d1e780c6814c4e03d7956
MinhWuan110 <0306221159@caothang.edu.vn>
2024-08-15 16:50:26 +0700

    fornend 15/8/2024

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -2,44 +2,44 @@ import React, { useState } from "react";
 import "./Login.css";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import {loginapi} from "../../service/api"
+import { loginapi } from "../../service/api";
 
 function Login(props) {
-
   const defaultcolor = {
-    checkColorValueLogin: true ,
-    checkColorPassword : true 
-  }
+    checkColorValueLogin: true,
+    checkColorPassword: true,
+  };
 
-  const [validValueLogin, setisvalidValueLogin] = useState("")
-  const [validPassword, setisvalidPassword] = useState("")
-  const [isCheckColorInput, setCheckColorInput] = useState(defaultcolor)
+  const [validValueLogin, setisvalidValueLogin] = useState("");
+  const [validPassword, setisvalidPassword] = useState("");
+  const [isCheckColorInput, setCheckColorInput] = useState(defaultcolor);
 
-
-  const navigate = useNavigate()
-  const handleNewAccount = () =>{
-    navigate("/Regester")
-  }
+  const navigate = useNavigate();
+  const handleNewAccount = () => {
+    navigate("/Regester");
+  };
 
   const handleLogin = async () => {
-    setCheckColorInput(defaultcolor)
-    if(!validValueLogin)
-    {
-      setCheckColorInput({...defaultcolor,checkColorValueLogin:false})
-      toast.error("Foget insert email or phone number ")
+    setCheckColorInput(defaultcolor);
+    if (!validValueLogin) {
+      setCheckColorInput({ ...defaultcolor, checkColorValueLogin: false });
+      toast.error("Foget insert email or phone number ");
       return;
     }
-    if(!validPassword)
-    {
-      toast.error("Foget insert Password ")
-      setCheckColorInput({...defaultcolor,checkColorPassword:false})
+    if (!validPassword) {
+      toast.error("Foget insert Password ");
+      setCheckColorInput({ ...defaultcolor, checkColorPassword: false });
       return;
     }
 
-    await loginapi(validValueLogin,validPassword)
-    navigate("/Home")
-
-  }
+    let login = await loginapi(validValueLogin, validPassword);
+    if (login && login.data && +login.data.EC === 0) {
+      navigate("/Home");
+    }
+    if (login && login.data && +login.data.EC !== 0) {
+      toast.error(login.data.EM);
+    }
+  };
 
   return (
     <div className="container">
@@ -49,40 +49,58 @@ function Login(props) {
             <h3 className="fw-bold"> Shop Minh Wuan </h3>{" "}
           </div>
           <div className="text-center">
-            
             <h3> Here we have many item about smartphone and laptop </h3>{" "}
           </div>
         </div>
         <div className="content-right  col-12 col-sm-5 d-flex flex-column mt-3 shadow p-3 mb-5 bg-body rounded">
           <div className="text-center d-sm-none">
-            
             <h3 className="fw-bold text-primary "> Shop Minh Wuan </h3>{" "}
           </div>
           <h2 className="text-center"> Sign in </h2>{" "}
           <input
             type="text"
-            className={isCheckColorInput.checkColorValueLogin ? "form-control mt-3" : " is-invalid form-control mt-3"} 
+            className={
+              isCheckColorInput.checkColorValueLogin
+                ? "form-control mt-3"
+                : " is-invalid form-control mt-3"
+            }
             placeholder="sign in by number phone or email "
             value={validValueLogin}
             onChange={(event) => setisvalidValueLogin(event.target.value)}
           ></input>
           <input
             type="password"
-            className={isCheckColorInput.checkColorPassword ? "form-control mt-3" : "is-invalid form-control mt-3" } 
+            className={
+              isCheckColorInput.checkColorPassword
+                ? "form-control mt-3"
+                : "is-invalid form-control mt-3"
+            }
             placeholder="import password "
             value={validPassword}
             onChange={(event) => setisvalidPassword(event.target.value)}
           ></input>
-          <a className="inline text-center mt-3" > Foget your passwoord </a> <hr />
-          <button className="btn btn-primary mb-3 " onClick={()=>handleLogin()}> Sign in </button>{" "}
+          <a className="inline text-center mt-3"> Foget your passwoord </a>{" "}
+          <hr />
+          <button
+            className="btn btn-primary mb-3 "
+            onClick={() => handleLogin()}
+          >
+            {" "}
+            Sign in{" "}
+          </button>{" "}
           <div className="text-center">
-            <button className="btn btn-success mb-3 " onClick={()=>handleNewAccount()}> Sign up </button>{" "}
+            <button
+              className="btn btn-success mb-3 "
+              onClick={() => handleNewAccount()}
+            >
+              {" "}
+              Sign up{" "}
+            </button>{" "}
           </div>
         </div>
       </div>
     </div>
   );
-  
 }
 
 export default Login;
--- src/service/api.js
+++ src/service/api.js
@@ -1,13 +1,13 @@
 import axios from "axios"
 
 const userapi = ( email, username, phone, password) => {
-    return axios.post("http://localhost:8080/api/v1/regester", {
+     return axios.post("http://localhost:8080/api/v1/regester", {
       email, username, phone, password
     })
 }
 
 const loginapi = (valueLogin, password) =>{
-   return axios.post("http://localhost:8080/api/v1/login", {
+    return axios.post("http://localhost:8080/api/v1/login", {
       valueLogin, password
     })
 }
