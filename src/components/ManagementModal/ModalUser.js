bde2aa949e7c4066177c19c3293e001b623c4c32
MinhWuan110 <0306221159@caothang.edu.vn>
2024-09-09 21:03:25 +0700

    9/9/2024

--- src/components/ManagementModal/ModalUser.js
+++ src/components/ManagementModal/ModalUser.js
@@ -2,9 +2,35 @@ import React from "react";
 import Modal from "react-bootstrap/Modal";
 import { useState, useEffect } from "react";
 import { fetchGroupapi } from "../../service/api";
-import { ListGroup } from "react-bootstrap";
+import _ from "lodash";
 
 function ModalUser(props) {
+  const defaultUserData = {
+    email: "",
+    phone: "",
+    username: "",
+    password: "",
+    sex: "",
+    group: "",
+  };
+
+  const defaultValidInput = {
+    email: true,
+    phone: true,
+    username: true,
+    password: true,
+    sex: true,
+    group: true,
+  };
+
+  const [userData, setUserData] = useState(defaultUserData);
+  const [validInput, setValidInput] = useState(defaultValidInput)
+
+  // const checkValidInput = () => {
+  //   setValidInput(defaultUserData)
+  //   let arr = ['email', 'phone', 'pass']
+  // }
+
   const [listGroup, setListGroup] = useState([]);
   useEffect(() => {
     fetchGroup();
@@ -15,6 +41,12 @@ function ModalUser(props) {
     if (respone && respone.data && respone.data.EC === 0) {
       setListGroup(respone.data.DT);
     }
+  };
+
+  const handleOnChangeInput = (value, name) => {
+    let _userData = _.cloneDeep(userData);
+    _userData[name] = value;
+    setUserData(_userData);
   };
 
   return (
@@ -32,8 +64,12 @@ function ModalUser(props) {
             </label>
             <input
               type="text"
-              className="form-control mt-3"
+              className={validInput.email ? "form-control mt-3" : "form-control mt-3 is-invalid"}
               placeholder="Your name "
+              value={userData.email}
+              onChange={(event) =>
+                handleOnChangeInput(event.target.value, "email")
+              }
             ></input>
           </div>
           <div>
@@ -41,7 +77,11 @@ function ModalUser(props) {
             <input
               type="text"
               className="form-control mt-3"
-              placeholder="Phone Number "
+              placeholder="your name "
+              value={userData.username}
+              onChange={(event) =>
+                handleOnChangeInput(event.target.value, "username")
+              }
             ></input>
           </div>
           <div>
@@ -50,6 +90,10 @@ function ModalUser(props) {
               type="text"
               className="form-control mt-3"
               placeholder="Phone Number "
+              value={userData.phone}
+              onChange={(event) =>
+                handleOnChangeInput(event.target.value, "phone")
+              }
             ></input>
           </div>
           <div>
@@ -58,22 +102,43 @@ function ModalUser(props) {
               type="Password"
               className="form-control mt-3"
               placeholder="Password "
+              value={userData.password}
+              onChange={(event) =>
+                handleOnChangeInput(event.target.value, "password")
+              }
             ></input>
           </div>
           <div>
             <label>Gender</label>
-            <select className="form-select" aria-label="Default select example">
+            <select
+              className="form-select"
+              aria-label="Default select example"
+              onChange={(event) =>
+                handleOnChangeInput(event.target.value, "sex")
+              }
+              // value={userData.sex}
+            >
               <option defaultValue>Male</option>
               <option>Female</option>
             </select>
           </div>
           <div>
             <label>Group</label>
-            <select className="form-select" aria-label="Default select example">
-                {listGroup.length > 0 && listGroup.map((item, index) =>{
-                    return (
-                        <option key={`group-${index}`} value={item.id}>{item.name}</option>
-                    )
+            <select
+              className="form-select"
+              aria-label="Default select example"
+              onChange={(event) =>
+                handleOnChangeInput(event.target.value, "group")
+              }
+              // value={userData.group}
+            >
+              {listGroup.length > 0 &&
+                listGroup.map((item, index) => {
+                  return (
+                    <option key={`group-${index}`} value={item.id}>
+                      {item.name}
+                    </option>
+                  );
                 })}
             </select>
           </div>
